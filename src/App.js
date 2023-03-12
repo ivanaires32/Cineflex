e10f90e0299928a706347496978656d24d65c066
Ivan <ivanaires32@gmail.com>
2023-03-11 21:57:06 -0300

    route aplicado

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import styled from "styled-components"
+import HomePage from "./pages/HomePage/HomePage"
+import SeatsPage from "./pages/SeatsPage/SeatsPage"
+import SessionsPage from "./pages/SessionsPage/SessionsPage"
+import SuccessPage from "./pages/SuccessPage/SuccessPage"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+export default function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <NavContainer>CINEFLEX</NavContainer>
+
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/horarios" element={<SessionsPage />} />
+          <Route path="/lugares" element={<SeatsPage />} />
+          <Route path="/finalizar" element={<SuccessPage />} />
+        </Routes>
+
+
+      </BrowserRouter>
+    </>
+  )
+}
+
+const NavContainer = styled.div`
+    width: 100%;
+    height: 70px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: #C3CFD9;
+    color: #E8833A;
+    font-family: 'Roboto', sans-serif;
+    font-size: 34px;
+    position: fixed;
+    top: 0;
+    a {
+        text-decoration: none;
+        color: #E8833A;
+    }
+`
